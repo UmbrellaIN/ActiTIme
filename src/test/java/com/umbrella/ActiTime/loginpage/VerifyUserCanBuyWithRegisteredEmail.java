4eec66da6ca8fe1117a362494746e493b1f3be1e
ankit <ankit@ankit-desktop>
2018-02-09 20:31:17 +0530

    Initial Commit

---
+++ src/test/java/com/umbrella/ActiTime/loginpage/VerifyUserCanBuyWithRegisteredEmail.java
@@ -0,0 +1,20 @@
+package com.umbrella.ActiTime.loginpage;
+
+import org.apache.log4j.Logger;
+import org.testng.annotations.Test;
+import com.umbrella.ActiTime.generics.LoggerHelper;
+import com.umbrella.ActiTime.testCore.TestBase;
+
+public class VerifyUserCanBuyWithRegisteredEmail extends TestBase {
+
+	private final Logger log = LoggerHelper.getLogger(VerifyUserCanBuyWithRegisteredEmail.class);
+
+	@Test(priority = 1)
+	public void testusercanloginwithvalidcredentials() throws Exception {
+		System.out.println(" **********I am starting Test of Demo site**********");
+		log.info(VerifyUserCanBuyWithRegisteredEmail.class + " started");
+		System.out.println("I am Just instantiating signinpage driver");
+
+	}
+
+}
